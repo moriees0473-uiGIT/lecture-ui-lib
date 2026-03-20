d7453a8526f847bf5f75584c1b908be8e8fe422d
moriees25 <moriees0473@gmail.com>
2026-03-20 19:38:11 +0900

    package up 2026/03/20 19:38

--- components/LectureCover.vue
+++ components/LectureCover.vue
@@ -1,50 +1,29 @@
 <script setup>
-
 const props = defineProps({
-  title: {
-    type: String,
-    default: 'Lecture Title'
-  },
-  description: {
-    type: String,
-    default: ''
-  },
-  items: {
-    type: Array,
-    default: () => []
-  }
+  title: { type: String, default: 'Lecture Title' },
+  description: { type: String, default: '' },
+  items: { type: Array, default: () => [] }
 })
 </script>
 
 <template>
-  <div class="relative h-full w-full flex flex-col justify-center px-12 py-10">
+  <div class="relative h-full w-full flex flex-col justify-center px-12 py-10 bg-[#001529] text-white">
+    
     <div class="absolute top-6 right-6">
-      <Link to="2" class="border-none text-3xl opacity-60 hover:opacity-100 transition-opacity cursor-pointer">
+      <Link to="setting" class="border-none text-2xl opacity-60 hover:opacity-100 transition-opacity cursor-pointer text-white">
         <carbon:settings />
       </Link>
     </div>
 
     <div class="max-w-3xl">
-      <h1 class="text-5xl font-bold mb-4 text-primary">
-        {{ title }}
-      </h1>
-      
-      <p v-if="description" class="text-xl opacity-80 mb-8 leading-relaxed">
-        {{ description }}
-      </p>
-
+      <h1 class="text-5xl font-bold mb-4 text-white">{{ title }}</h1>
+      <p v-if="description" class="text-xl opacity-80 mb-8 leading-relaxed">{{ description }}</p>
       <ul v-if="items.length" class="space-y-3">
         <li v-for="(item, index) in items" :key="index" class="flex items-center text-lg">
-          <span class="mr-3 text-primary">●</span>
+          <span class="mr-3 text-blue-400">●</span>
           {{ item }}
         </li>
       </ul>
     </div>
   </div>
 </template>
-
-<style scoped>
-.text-primary {
-  color: var(--slidev-theme-primary);
-}
-</style>
